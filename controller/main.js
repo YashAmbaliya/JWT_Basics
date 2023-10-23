06360c7e3ecb31d850e638881d96245c2e81def8
YashAmbaliya <yashambaliya266@gmail.com>
2023-10-24 00:12:19 +0530

    first commit

---
+++ controller/main.js
@@ -0,0 +1,36 @@
+// Check username, password in post(login) request
+// if exist create JWT
+// send back to front-end
+
+// Setup authentication so only the request with JWT can access the dashboard
+
+const jwt = require('jsonwebtoken');
+const {BadRequestError} = require('../errors');
+
+const login = async (req, res) => {
+    const {username, password} = req.body;
+    // Mongoose validation
+    // Joi
+    // Check in the controller
+
+    if(!username || !password){
+        throw new BadRequestError('Please provide username and password!!');
+    }
+
+    // Just for demo, normally provided by DB!!!
+    const id = new Date().getDate();
+
+    // Try to keep payload small, better experience for user
+    // Just for demo, in production use long, complex and unguessable string value!!!!
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'});
+    res.status(200).json({msg: 'User created!!', token});
+};
+
+const dashboard = async (req, res) => {
+    console.log(req.user);
+    const luckyNumber = Math.floor(Math.random()*100);
+        res.status(200).json({msg: `Hello, ${req.user.username}.`, secret: `Here is your authorised data, your lucky number is ${luckyNumber}`});
+    // console.log(req.headers);
+};
+
+module.exports = {login, dashboard};
